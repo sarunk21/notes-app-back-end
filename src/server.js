a6f308ba7dac906057490deb768c4b9fd1ff64c5
sarunk <kautsarmkp@gmail.com>
2022-09-22 15:20:30 +0700

    server-side caching implemented

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const Inert = require('@hapi/inert');
-const path = require('path');
 
 // Notes
 const notes = require('./api/notes');
@@ -37,9 +36,13 @@ const uploads = require('./api/uploads');
 const StorageService = require('./service/S3/StorageService');
 const UploadsValidator = require('./validator/uploads');
 
+// cache
+const CacheService = require('./service/redis/CacheService');
+
 const init = async () => {
-  const collaborationsService = new CollaborationsService();
-  const notesService = new NotesService(collaborationsService);
+  const cacheService = new CacheService();
+  const collaborationsService = new CollaborationsService(cacheService);
+  const notesService = new NotesService(collaborationsService, cacheService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
   const storageService = new StorageService();
